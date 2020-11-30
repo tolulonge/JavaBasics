aa292dd8b1bd38f899a4f61569a9ed258b61a7ef
Longe Tolulope Adeola <tolulopelonge945@gmail.com>
2020-11-30 11:55:55 +0000

    renamed variables

--- Main.java
+++ Main.java
@@ -4,12 +4,13 @@ class Main {
     byte b = 2;
     int i = 345;
     Boolean boo = true;
-    short s = 23;
-    float f = 3.4f;
-    double d = 4.567;
-    long l = 45686884743L;
-    String st = "Java";
-    boolean no = false;
+    short bags = 23;
+    float length = 3.4f;
+    double area = 4.567;
+    long price = 45686884743L;
+    char letter = 't';
+    String course = "Java";
+    boolean isOpen = false;
     System.out.print(Tolulope.haveEaten(null));
 
   }
